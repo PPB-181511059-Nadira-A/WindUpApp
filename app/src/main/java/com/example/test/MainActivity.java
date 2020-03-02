f36d2dda3d2788177800b4c254f0f8b343b2fac3
Nadira Arasya <nadira.arasya.tif18@polban.ac.id>
2020-03-02 14:33:55 +0700

    Tampilan utama awal

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.test;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.recyclerview.RecyclerViewAdapter;
+import com.example.test.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataSet;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dataSet = new ArrayList<>();
+        initDataset();
+
+        rvView = (RecyclerView) findViewById(R.id.rv_main);
+        rvView.setHasFixedSize(true);
+
+        /**
+         * Kita menggunakan LinearLayoutManager untuk list standar
+         * yang hanya berisi daftar item
+         * disusun dari atas ke bawah
+         */
+        layoutManager = new LinearLayoutManager(this);
+        rvView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerViewAdapter(dataSet);
+        rvView.setAdapter(adapter);
+
+    }
+
+    private void initDataset(){
+
+        /**
+         * Tambahkan item ke dataset
+         * dalam prakteknya bisa bermacam2
+         * tidak hanya String seperti di kasus ini
+         */
+        dataSet.add("Membuat Paper");
+        dataSet.add("Kumpul Himpunan");
+        dataSet.add("Mengerjakan tugas APSI");
+        dataSet.add("Rapat Departemen");
+        dataSet.add("Mengerjakan proposal PKM");
+        dataSet.add("Membuat DFD");
+        dataSet.add("Membaca Chapter 3 Software Engineering");
+        dataSet.add("Mengerjakan tugas Proyek");
+        dataSet.add("Mengerjakan tugas Matematika Diskrit");
+    }
+}
